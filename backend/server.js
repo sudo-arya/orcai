204ef0f2b8f7e59b75fd1a7b805e897722dbd9fb
Deepanshu Arya <whoami.arya@gmail.com>
2025-02-12 20:18:48 +0530

    continuously running

--- backend/server.js
+++ backend/server.js
@@ -120,4 +120,5 @@ app.get("/images", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
